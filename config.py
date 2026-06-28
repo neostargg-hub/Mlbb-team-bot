5373d13a2c26c9ec84415383fc03f84143ee6f9e
neostargg-hub <neostar.gg@gmail.com>
2026-06-28 08:08:34 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
-WEBAPP_URL = os.getenv("WEBAPP_URL")
-OWNER_ID = int(os.getenv("OWNER_ID", 5391287151))
