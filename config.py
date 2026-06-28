094a8fb0c00d7e1b55269478880b8e81475f2d5c
neostargg-hub <neostar.gg@gmail.com>
2026-06-28 23:03:41 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-SITE_URL = os.getenv("SITE_URL", "https://your-site.onrender.com")
-OWNER_ID = int(os.getenv("OWNER_ID", 5391287151))
